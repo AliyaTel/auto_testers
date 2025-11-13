7fc7a6cd88241cf87aaa07ac963ae17a6112a0ac
alfiya <magarova2004@gmail.com>
2025-11-13 21:16:04 +0600

    home4

--- homework/homework_4.py
+++ homework/homework_4.py
@@ -13,23 +13,61 @@
 #print (len(friend["favorite_colors"]),)
 #print (friend["favorite_colors"])
 
-fio = input("Введите ваши данные, в формате: имя, возраст, город")
-print(fio)
+# fio = input("Введите ваши данные, в формате: имя, возраст, город")
+# print(fio)
+#
+# parts = fio.split(", ")
+# print(parts)
+#
+# name, age, city = parts
+# print(name)
+# print(age)
+# print(city)
+#
+# Name_2 = name[:2]
+# print(Name_2)
+#
+# age_2 = int(age[:2])
+# print(type(age_2))
+#
+# city_2 = city.capitalize()
+# print(city_2)
+# print(f"Привет, {name}! Тебе {age} лет, Ты из города {city}")
 
-parts = fio.split(", ")
-print(parts)
+# schedule = ["Математика", "08:00 - 08:45", "Каб. 12"]
+#
+# parts = schedule
+# print(parts)
+#
+# math, time, rest = parts
+# print(math, time, rest)
+#
+# a = "math"
+# b = "time"
+# c = "rest"
+#
+# my_text = "First lesson is {0}, second lesson is {1}, third lesson is {2}"
+# print(my_text.format(a, b, c))
+#
+# my_text = f"First lesson is {c}, second lesson is {a}, third lesson is {b}"
+# print(my_text)
 
-name, age, city = parts
-print(name)
-print(age)
-print(city)
+# user_input = int(input("Оцените: "))
+#
+# if user_input == 4 or user_input == 5:
+#     print("хорошо")
+# elif user_input == 3:
+#     print("Можно лучше")
+# elif user_input == 2 or user_input == 1:
+#     print("нужно подтянуться")
 
-Name_2 = name[:2]
-print(Name_2)
+# text = "Мама мыла раму, папа читал газету, а кот спал на диване"
+#
+# item = text.split(" ")
+#
+# for i in item:
+#     if "а" in i:
+#         print(f"Слово {i} содержит А")
+#     else:
+#         print(f"Слово {i} не содержит А")
 
-age_2 = int(age[:2])
-print(type(age_2))
-
-city_2 = city.capitalize()
-print(city_2)
-print(f"Привет, {name}! Тебе {age} лет, Ты из города {city}")
