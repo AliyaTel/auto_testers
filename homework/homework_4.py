327bbc610eacb8b7a0c27103e2e3e8dfd2dd89c4
alfiya <magarova2004@gmail.com>
2025-11-20 17:01:00 +0600

    дз

--- homework/homework_4.py
+++ homework/homework_4.py
@@ -71,3 +71,22 @@
 #     else:
 #         print(f"Слово {i} не содержит А")
 
+secretNumber = 7
+guess = None
+while guess != secretNumber:
+     guess = int(input("Введите число:  "))
+     if guess != secretNumber:
+         print("Попробуй еще ")
+print("поздравляем, верно!")
+
+text = "Мама мыла раму, папа читал газету, а кот спал на диване"
+words = text.split()
+count_a = 0
+i = 0
+while i < len(words):
+    word = words[i]
+    if "а" in word.lower():
+        count_a += 1
+    i += 1
+print(f"количество слов с буквой а: {count_a}")
+
