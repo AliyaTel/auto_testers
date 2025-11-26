e3ace22da1fd5cc682c5fc92a4741895c6412d20
alfiya <magarova2004@gmail.com>
2025-11-27 02:04:32 +0600

    home_6

--- homework/homework_5.py
+++ homework/homework_5.py
@@ -57,5 +57,97 @@ text = "Мама мыла раму, папа читал газету, а кот 
 analyze_sentence(text)
 
 
+class Animal:
+    def __init__(self, name, owner, age):
+        self.name = name
+        self.owner = owner
+        self.age = age
+
+    def voice(self):
+        print(gav)
+
+    def eat(self):
+        print(food)
+
+
+class Cat(Animal):
+
+    def __init__(self, name, owner, age, mouse):
+        super().__init__(name, owner, age)
+        self.mouse = mouse
+
+    def voice(self):
+        print("meu")
+
+    def eat(self):
+        print("cookies")
+
+
+class Dog(Animal):
+
+    def __init__(self, name, owner, age, colors):
+        super().__init__(name, owner, age)
+        self.colors = colors
+
+    def voice(self):
+        print("gav")
+
+    def eat(self):
+        print("bread")
+
+
+cat_1 = Cat("Barsik", "Andrey", 2, "yes")
+cat_1.eat()
+cat_1.voice()
+print(cat_1.name)
+print(cat_1.owner)
+print(cat_1.age)
+print(cat_1.mouse)
+
+
+dog_2 = Dog("Sharik", "Viktor", 4, "white")
+dog_2.voice()
+dog_2.eat()
+print(dog_2.name)
+print(dog_2.owner)
+print(dog_2.age)
+print(dog_2.colors)
+
+file = open("home_data1.txt", "r")
+data = file.readlines()
+for line in data:
+    line = line.strip() #не обяз строка (просто есть уже)
+    print(line)
+
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_info(self):
+        return f"Название {self.title}, Автор {self.author}, Год {self.year}"
+
+
+class Fiction(Book):
+    def get_info(self):
+        return f"Название {self.title}, Автор {self.author}, Год {self.year}"
+
+
+class NonFiction(Book):
+    def get_info(self):
+        return f"Название: {self.title}, Автор: {self.author}, Год: {self.year}, Жанр: Non-Fiction"
+
+
+book1 = Fiction("1984", "Джордж Оруэлл", 1949)
+book2 = Fiction("Мастер и Маргарита", "М. Булгаков", 1967)
+book3 = NonFiction("Краткая история времени", "Стивен Хокинг", 1988)
+book4 = NonFiction("Самая большая тайна", "Дэвид Айк", 1999)
+
+books = [book1, book2, book3, book4]
+
+for book in books:
+    print(book.get_info())
 
 
