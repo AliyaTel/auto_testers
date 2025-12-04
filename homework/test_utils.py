0a2f3e8a6332ffbf070c5ce63a48466cbe2cec9d
alfiya <magarova2004@gmail.com>
2025-12-04 22:42:17 +0600

    utils
    # Conflicts:
    #	homework/utils.py

--- homework/test_utils.py
+++ homework/test_utils.py
@@ -1,27 +1,65 @@
 import pytest
-from utils import get_max, get_min, get_average
+import requests
+# from utils import get_max, get_min, get_average
+#
+#
+# def test_get_max():
+#     assert get_max([1, 5, 3]) == 5
+#     assert get_max([-10, -3, -20]) == -3
+#
+#     with pytest.raises(ValueError):
+#         get_max([])
+#
+#
+# def test_get_min():
+#     assert get_min([4, 2, 9]) == 2
+#     assert get_min([-5, -1, -9]) == -9
+#
+#     with pytest.raises(ValueError):
+#         get_min([])
+#
+#
+# def test_get_average():
+#     assert get_average([2, 4, 6]) == 4
+#     assert get_average([1]) == 1
+#     assert get_average([-3, 3]) == 0
+#
+#     with pytest.raises(ValueError):
+#         get_average([])
 
 
-def test_get_max():
-    assert get_max([1, 5, 3]) == 5
-    assert get_max([-10, -3, -20]) == -3
-
-    with pytest.raises(ValueError):
-        get_max([])
-
-
-def test_get_min():
-    assert get_min([4, 2, 9]) == 2
-    assert get_min([-5, -1, -9]) == -9
-
-    with pytest.raises(ValueError):
-        get_min([])
+@pytest.fixture()
+def new_post():
+    body = {
+        "title": "lalalala",
+        "body": "lala",
+        "userId": 1
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post('https://jsonplaceholder.typicode.com/posts', json=body, headers=headers)
+    yield response.json()['id']
+    requests.delete(f'https://jsonplaceholder.typicode.com/posts/42')
 
 
-def test_get_average():
-    assert get_average([2, 4, 6]) == 4
-    assert get_average([1]) == 1
-    assert get_average([-3, 3]) == 0
+def test_all_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts').json()
+    assert len(response) == 100, 'not all posts'
 
-    with pytest.raises(ValueError):
-        get_average([])
+
+@pytest.mark.skip
+def test_one_post(new_post):
+    post_id = new_post()
+    response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{post_id}').json()
+    assert response['id'] == post_id
+
+
+@pytest.mark.regress
+def test_post_a_post():
+    body = {
+        "title": "lalalala",
+        "body": "lala",
+        "userId": 1
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post('https://jsonplaceholder.typicode.com/posts', json=body, headers=headers)
+    assert response.status_code == 201
--- homework/utils.py
+++ homework/utils.py
@@ -1,18 +1,109 @@
+import requests
+import json
+#
+# def get_max(numbers):
+#     if not numbers:
+#         raise ValueError("Список не должен быть пустым")
+#     return max(numbers)
+#
+#
+# def get_min(numbers):
+#     if not numbers:
+#         raise ValueError("Список не должен быть пустым")
+#     return min(numbers)
+#
+#
+# def get_average(numbers):
+#     if not numbers:
+#         raise ValueError("Список не должен быть пустым")
+#     return sum(numbers) / len(numbers)
+#
 
 
-def get_max(numbers):
-    if not numbers:
-        raise ValueError("Список не должен быть пустым")
-    return max(numbers)
+# def all_posts():
+#     # response = requests.request('Get', 'https://jsonplaceholder.typicode.com/posts')
+#     response = requests.get('https://jsonplaceholder.typicode.com/posts').json()
+#     # print(response.text)
+#     # print(response.json()[0])
+#     assert len(response) == 100, 'not all posts'
+#
+#
+# def new_post():
+#     body = {
+#         "title": 'lalalala',
+#         "body": 'lala',
+#         "UserID": 1
+#     }
+#     headers = {'Content-Type': 'application/json'}
+#     response = requests.post('https://jsonplaceholder.typicode.com/posts', json=body, headers=headers)
+#     return response.json()['id']
+#
+# def delete():
+#     response = requests.delete(f'https://jsonplaceholder.typicode.com/posts/42')
+#     assert response.status_code == 200
+#
+# def one_post():
+#     post_id = new_post()
+#     response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{post_id}').json()
+#     print(response)
+#     assert response['id'] == post_id
+#     delete()
+#
+# def post_a_post():
+#     body = {
+#         "title": 'lalalala',
+#         "body": 'lalala',
+#         "UserID": 1
+#     }
+#     headers = {'Content-Type': 'application/json'}
+#     response = requests.post('https://jsonplaceholder.typicode.com/posts', json=body, headers=headers)
+#     # print(response.status_code)
+#     assert response.status_code == 201
+#
+# all_posts()
+# post_a_post()
+#
+#
+# def get_coordinates():
+#     url = "https://geocoding-api.open-meteo.com/v1/search"
+#     param = {
+#         'name': 'New York',
+#         "count" : 1
+#     }
+#     response = requests.get(url, params=param)
+#     data = response.json()
+#     print(data['results'])
+#
+# get_coordinates()
+#
+#
+# def get_meteo():
+#     url = "https://api.open-meteo.com/v1/forecast"
+#     params = {
+#         'latitude': 40.71427,
+#         'longitude': -74.00597,
+#         "current_weather": True
+#     }
+#     response = requests.get(url, params=params)
+#     data = response.json()
+#     print(data)
+#     print (data ["generationtime_ms"])
+#
+# get_meteo()
+#
+#
+#
 
 
-def get_min(numbers):
-    if not numbers:
-        raise ValueError("Список не должен быть пустым")
-    return min(numbers)
+def get_facts():
+    url = "https://catfact.ninja/fact"
 
+    fact_1 = requests.get(url).json().get("fact")
+    fact_2 = requests.get(url).json().get("fact")
+    fact_3 = requests.get(url).json().get("fact")
 
-def get_average(numbers):
-    if not numbers:
-        raise ValueError("Список не должен быть пустым")
-    return sum(numbers) / len(numbers)
+    facts = {fact_1, fact_2, fact_3}
+
+    print(facts)
+
+get_facts()
