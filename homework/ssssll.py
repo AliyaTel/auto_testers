725af8f8ebc5254df0b3d7baff3e04caeed6fcd6
alfiya <magarova2004@gmail.com>
2025-12-19 15:01:39 +0600

    sss

--- homework/ssssll.py
+++ homework/ssssll.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -15,35 +18,39 @@ def driver():
     yield browser
     sleep(100)
 
-# def test_find_by_name(driver):
-#     driver.get("https://www.google.com/?hl=ru")
-#     text_area = driver.find_element(By.NAME, "q")
-#     text_input = "cat"
-#     text_area.send_keys(text_input)
-#     text_area.send_keys(Keys.ENTER)
-#     assert driver.title == text_input
-#
-#
-# def test_find_by_id(driver):
-#     driver.get("https://www.qa-practice.com/elements/input/simple")
-#     text_input = driver.find_element(By.ID,"id_text_string")
-#     text_test = "test"
-#     text_input.send_keys(text_test)
-#     text_input.send_keys(Keys.ENTER)
-#     text_result = driver.find_element(By.CLASS_NAME,"result-text")
-#     assert text_result.text == text_test
+def test_find_by_name(driver):
+    driver.get("https://www.google.com/?hl=ru")
+    text_area = driver.find_element(By.NAME, "q")
+    text_input = "cat"
+    text_area.send_keys(text_input)
+    text_area.send_keys(Keys.ENTER)
+    assert driver.title == text_input
 
 
-def test_email_field(driver):
-    driver.get("https://www.qa-practice.com/elements/input/email")
+def test_find_by_id(driver):
+    driver.get("https://www.qa-practice.com/elements/input/simple")
+    text_input = driver.find_element(By.ID,"id_text_string")
+    text_test = "test"
+    text_input.send_keys(text_test)
+    text_input.send_keys(Keys.ENTER)
+    text_result = driver.find_element(By.CLASS_NAME,"result-text")
+    assert text_result.text == text_test
 
-    email_input = driver.find_element(By.ID, "id_email")
-    valid_email = "user@example.com"
 
-    email_input.send_keys(valid_email)
-    email_input.send_keys(Keys.ENTER)
+def test_first_multiselect(driver):
+    driver.get('https://www.qa-practice.com/elements/select/mult_select')
+    select = driver.find_element(By.NAME, "choose_the_place_you_want_to_go")
+    location_select = Select(select)
+    location_select.select_by_value("4")
+    assert location_select.first_selected_option.is_selected()
 
-    result = driver.find_element(By.CLASS_NAME, "result-text")
-    assert result.text == valid_email
+
+def test_find_by_css_selector(driver):
+    driver.get("https://www.qa-practice.com/elements/select/mult_select")
+    select_element = driver.find_element(By.CSS_SELECTOR, "select[name ='choose_how_you_want_to_get_there']")
+    transport_select = Select(select_element)
+    transport_select.select_by_value("2")
+    assert transport_select.first_selected_option.is_selected()
+
 
 
